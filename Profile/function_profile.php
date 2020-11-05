25c208c53d1a835bdb6d90eb3f64fa455dc8756a
@Reascer <leonropgop@gmail.com>
2020-11-05 21:57:08 +0100

    i'm dead T-T

--- Profile/function_profile.php
+++ Profile/function_profile.php
@@ -2,7 +2,12 @@
     function getQuestsValidate()
     {
         require "../includes/BDDConnection.php";
-        $requete = $bdd->prepare("SELECT nom_quetes,validation_quetes.date_fin_quetes,validation_amount from validation_quetes,quetes_utilisateur,quetes where quetes_utilisateur.id_quetes = quetes.id_quetes AND quetes_utilisateur.id_utilisateur = :id AND quetes_utilisateur.etat_quetes_utilisateur = 1");
+        $requete = $bdd->prepare("SELECT nom_quetes,validation_quetes.date_fin_quetes,validation_amount FROM validation_quetes,quetes_utilisateur,quetes 
+        WHERE quetes_utilisateur.id_quetes = quetes.id_quetes 
+        AND quetes_utilisateur.id_quetes = validation_quetes.id_quetes 
+        AND quetes_utilisateur.id_utilisateur = :id 
+        AND quetes_utilisateur.etat_quetes_utilisateur = 1");
+
         $requete->bindParam(':id', $_SESSION['id']);
         // exécute
         $requete->execute();
